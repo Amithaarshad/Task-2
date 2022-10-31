3cb4ea8d7d2a556d1f404fde404ad240e6092535
amitharshad <amitaarshad@01>
2022-10-31 16:57:59 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
-import Dashboard from './components/screens/Dashboard'
-import Right from './components/screens/Right'
-import Wallet from './components/screens/Wallet'
-import "./App.css" 
+import React from "react";
+import styled from "styled-components";
+import Dashboard from "./components/screens/Dashboard";
+import Right from "./components/screens/Right";
+import "./App.css";
+import Wallet from "./components/screens/Wallet";
 function App() {
   return (
     <>
       <Container>
-        <Wallet/>
-        <Dashboard/>
-        <Right/>
+        <Wallet />
+        <Dashboard />
+        <Right />
       </Container>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
 
 const Container = styled.div`
   display: flex;
--- src/components/screens/wallet.js
+++ src/components/screens/wallet.js
@@ -1,37 +1,98 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function Wallet() {
   return (
-      <Maincontainer>
-        <Wrapper className="wrapper">
-          <Imagecontainer>
-            <Logoimg src={require("../assets/images/logo-wallet.png")}></Logoimg>
-          </Imagecontainer>
-          <Lists>
-            <Items>
-              <Itemlogo>
-                <Img src={require("../assets/images/property-1=menu.svg").default}></Img>
-              </Itemlogo>
-              <Itemtype>
-                <Itemtitle>Dashboard</Itemtitle>
-              </Itemtype>
-            </Items>
-            </Lists>
-         </Wrapper>   
-      </Maincontainer>
-  )
+    <Maincontainer>
+      <Wrapper className="wrapper">
+        <Imagecontainer>
+          <Logoimg src={require("../assets/images/logo-wallet.png")} />
+        </Imagecontainer>
+        <Lists>
+          <Items>
+            <Itemlogo>
+              <Img
+                src={require("../assets/images/property-1=menu.svg").default}
+              ></Img>
+            </Itemlogo>
+            <Itemtype>
+              <Itemtitle>Dashboard</Itemtitle>
+            </Itemtype>
+          </Items>
+          <Items>
+            <Itemlogo>
+              <Img
+                src={
+                  require("../assets/images/property-1=credit-card.svg").default
+                }
+              ></Img>
+            </Itemlogo>
+            <Itemtype>
+              <Itemtitle>My Cards</Itemtitle>
+            </Itemtype>
+          </Items>
+          <Items>
+            <Itemlogo>
+              <Img
+                src={require("../assets/images/property-1=lock.svg").default}
+              ></Img>
+            </Itemlogo>
+            <Itemtype>
+              <Itemtitle>Savings</Itemtitle>
+            </Itemtype>
+          </Items>
+          <Items>
+            <Itemlogo>
+              <Img
+                src={
+                  require("../assets/images/property-1=transaction.svg").default
+                }
+              ></Img>
+            </Itemlogo>
+            <Itemtype>
+              <Itemtitle>Transaction</Itemtitle>
+            </Itemtype>
+          </Items>
+          <Items>
+            <Itemlogo>
+              <Img
+                src={require("../assets/images/property-1=gear.svg").default}
+              ></Img>
+            </Itemlogo>
+            <Itemtype>
+              <Itemtitle>Settings</Itemtitle>
+            </Itemtype>
+          </Items>
+        </Lists>
+      </Wrapper>
+      <BottomItems>
+        <Itemlogo>
+          <Img src={require("../assets/images/rectangle.jpg")}></Img>
+        </Itemlogo>
+        <Itemtype>
+          <Itemtitle>
+            Jennifer <br /> Connectify
+          </Itemtitle>
+        </Itemtype>
+        <Dot>
+          <Img
+            src={require("../assets/images/property-1=dot.svg").default}
+          ></Img>
+        </Dot>
+      </BottomItems>
+    </Maincontainer>
+  );
 }
 
-export default Wallet
-
+export default Wallet;
 
 const Maincontainer = styled.section`
   width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
-const Wrapper = styled.div`
-
-`;
+const Wrapper = styled.div``;
 const Imagecontainer = styled.h1`
   display: inline-block;
   width: 180px;
@@ -43,10 +104,24 @@ const Logoimg = styled.img`
 const Lists = styled.ul``;
 const Items = styled.li`
   display: flex;
-  justify-content: space-between;
-  width: 70%;
+  padding-top: 20px;
+  margin-left: 25px;
 `;
 const Itemlogo = styled.div``;
-const Img = styled.img``;
+const Img = styled.img`
+  width: 70%;
+`;
 const Itemtype = styled.div``;
-const Itemtitle = styled.h3``;
+const Itemtitle = styled.h3`
+  margin-left: 10px;
+  font-size: 15px;
+`;
+const Dot = styled.div`
+  width: 100%;
+  margin-left: 25%;
+`;
+const BottomItems = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  margin-left: 30px;
+`;
